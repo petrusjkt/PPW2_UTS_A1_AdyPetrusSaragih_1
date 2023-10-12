573ca0e4f66eb6b53563913c201d1a9e59446ac1
petrusjkt <adypetrussaragih@mail.ugm.ac.id>
2023-10-12 09:05:49 +0700

    uts ppw2

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Nama Proyek Anda</title>
+</head>
+<body>
+    <header>
+        <!-- Isi elemen header di sini -->
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <footer>
+        <!-- Isi elemen footer di sini -->
+    </footer>
+</body>
+</html>
